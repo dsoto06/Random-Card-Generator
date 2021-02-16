13f75fd639cf4aae71711c5c86bc468cf1ce35ab
dsoto06 <diegosr06@hotmail.com>
2021-02-16 20:55:52 +0000

    entrega card generator

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let symbol = ["♣", "♦", "♥", "♠"];
+let number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"];
+
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let random1 = symbol[Math.floor(Math.random() * symbol.length)];
+  let random2 = number[Math.floor(Math.random() * number.length)];
+  let random3 = random1;
+
+  document.querySelector("#symbol").innerHTML = random1;
+  document.querySelector("#number").innerHTML = random2;
+  document.querySelector("#symbol1").innerHTML = random3;
 };
